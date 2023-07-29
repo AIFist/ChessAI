d0b0e8c848ae0064731d42e768ee994398f8c518
MuhammadNizamani <ishaque.niz786@gmail.com>
2023-07-29 18:06:39 +0500

    ENH: Piece Class is finshed

---
+++ Chess Board/src/board.py
@@ -0,0 +1,16 @@
+from const import *
+from square import Square
+class Board:
+    def __init__(self):
+        self.squares = [[0, 0, 0, 0, 0, 0, 0, 0] for col in range(COLS)]
+        self._create()
+        
+    def _create(self):
+        for row in range(ROWS):
+            for col in range(COLS):
+                self.squares[row][col] = Square(row, col)
+    
+    def _add_piece(self):
+        pass
+                
+            
---
+++ Chess Board/src/piece.py
@@ -0,0 +1,49 @@
+import math
+import os
+class Piece:
+    def __init__(self, name, color, value, texture = None, texture_rect= None):
+        self.name = name
+        self.color = color
+        value_sign = 1 if color =='white' else -1
+        self.value = value * value_sign
+        self.moves = []
+        self.moved = False
+        self.texture = texture
+        self.set_texture()
+        self.texture_rect = texture_rect
+    
+    def set_texture(self, size = 80):
+        self.texture = os.path.join(
+            f'assets/images/img-{size}px/[{self.color}_{self.name}.png'
+        )
+    def add_move(self, move):
+        self.moves.append(move)
+    
+    
+
+
+class Pawn(Piece):
+    def __init__(self,color):
+        self.dir = -1 if color == 'white' else 1
+        super().__init__('pawn', color, 1.0)
+        
+
+class Knight(Piece):
+    def __init__(self,color):
+        super().__init__('knight', color, 3.0001)
+        
+class Bishop(Piece):
+    def __init__(self,color):
+        super().__init__('bishop', color, 3.0)
+    
+class Rook(Piece):
+    def __init__(self,color):
+        super().__init__('rook', color, 5.0)
+        
+class Queen(Piece):
+    def __init__(self,color):
+        super().__init__('queen', color, 9.0)
+        
+class King(Piece):
+    def __init__(self,color):
+        super().__init__('king', color, math.inf)
