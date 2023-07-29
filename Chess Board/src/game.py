fc6ccb7060cdf15daab47d782d2a61c68b5216d1
MuhammadNizamani <ishaque.niz786@gmail.com>
2023-07-29 16:27:07 +0500

    EHN: Empty chess Board is created

---
+++ Chess Board/src/game.py
@@ -0,0 +1,17 @@
+import pygame
+from const import *
+class Game:
+    def __init__(self) -> None:
+        pass
+    
+    def show_bg(self, surface):
+        for row in range(ROWS):
+            for col in range(COLS):
+                if (row+col) %2 ==0:
+                    color = (234,235, 200) # light green
+                else:
+                  color = (119,154,88) # dark green
+                
+                rect = (col*SQSIZE, row*SQSIZE, SQSIZE, SQSIZE)
+                
+                pygame.draw.rect(surface, color, rect)
