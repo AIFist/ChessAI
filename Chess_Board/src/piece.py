9af95136d8fa89ea8864deb968efe4c06c811f41
MuhammadNizamani <ishaque.niz786@gmail.com>
2023-08-01 17:34:33 +0500

    EHN: renaming the folder

--- Chess_Board/src/piece.py
+++ Chess_Board/src/piece.py
@@ -14,7 +14,7 @@ class Piece:
     
     def set_texture(self, size = 80):
         self.texture = os.path.join(
-            f'/home/ishaquenizamani/work/opensourse/ishaque projecr/ChessAI/Chess Board/assets/images/img-{size}px/{self.color}_{self.name}.png'
+            f'./Chess_Board/assets/images/img-{size}px/{self.color}_{self.name}.png'
         )
     def add_move(self, move):
         self.moves.append(move)
