d187e80ad3f25d198eb8550f12d1e4406f5c425c
MuhammadNizamani <ishaque.niz786@gmail.com>
2023-08-06 12:53:49 +0500

    EHN: moving pieces are aded

--- Chess_Board/src/move.py
+++ Chess_Board/src/move.py
@@ -3,3 +3,14 @@ class Move:
         # initial and final are squres
         self.initial = initial
         self.final = final
+        
+    def __str__(self) :
+        s = ''
+        s += f'({self.initial.col},{self.initial.row})'
+        s += f' -> ({self.final.col},{self.final.row})'
+        return s
+    
+    def __eq__(self, other):
+        return self.initial == other.initial and self.final == other.final
+    
+    
