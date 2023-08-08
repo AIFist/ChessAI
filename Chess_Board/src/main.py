7e918f3c71ae8bb7140659a5fcd862ee7136cefd
MuhammadNizamani <ishaque.niz786@gmail.com>
2023-08-08 19:35:46 +0500

    EHN: show_hover and show_last_move is added

--- Chess_Board/src/main.py
+++ Chess_Board/src/main.py
@@ -19,8 +19,10 @@ class Main:
         
         while True:
             game.show_bg(screen)
+            game.show_last_move(screen)
             game.show_moves(screen)
             game.show_piece(screen)
+            game.show_hover(screen)
             
             if dragger.dragging:
                 dragger.update_blit(screen)
@@ -44,6 +46,7 @@ class Main:
                             dragger.drag_piece(piece)
                             # show methos
                             game.show_bg(screen)
+                            game.show_last_move(screen)
                             game.show_moves(screen)
                             game.show_piece(screen)
                             
@@ -51,12 +54,17 @@ class Main:
                 
                 #mouse motion
                 elif event.type == pygame.MOUSEMOTION:
+                    motion_row = event.pos[1]  // SQSIZE
+                    motion_col = event.pos[0]  // SQSIZE
+                    game.set_hover(motion_row, motion_col)
                     if dragger.dragging:
                         dragger.update_mouse(event.pos)
                         # show method
                         game.show_bg(screen)
+                        game.show_last_move(screen)
                         game.show_moves(screen)
                         game.show_piece(screen)
+                        game.show_hover(screen)
                         dragger.update_blit(screen)
                 #Click release
                 elif event.type == pygame.MOUSEBUTTONUP:
@@ -77,6 +85,7 @@ class Main:
                             board.move(dragger.piece, move)
                             # show methods
                             game.show_bg(screen)
+                            game.show_last_move(screen)
                             game.show_piece(screen)
                             # next turn
                             game.next_turn()
