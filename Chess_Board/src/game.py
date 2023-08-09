f01a50b93565e8400c074a0272ac4d3ba4992669
MuhammadNizamani <ishaque.niz786@gmail.com>
2023-08-09 13:11:29 +0500

    ENH: game congies ared added

--- Chess_Board/src/game.py
+++ Chess_Board/src/game.py
@@ -3,6 +3,7 @@ from const import *
 from board import Board
 from dragger import Dragger
 from config import Config
+from square import Square
 class Game:
     def __init__(self) -> None:
         self.next_player = 'white'
@@ -21,6 +22,25 @@ class Game:
                 rect = (col*SQSIZE, row*SQSIZE, SQSIZE, SQSIZE)
                 # blit
                 pygame.draw.rect(surface, color, rect)
+                
+                # row coordinate
+                if col == 0:
+                    # color
+                    color = theme.bg.dark  if row % 2 == 0 else theme.bg.light
+                    # label 
+                    lbl = self.config.font.render(str(ROWS - row), 1, color)
+                    lbl_pos = (5, 5 + row * SQSIZE)
+                    surface.blit(lbl, lbl_pos)
+                    
+                # col coordinate 
+                if row == 7:
+                    # color
+                    color = theme.bg.dark  if (row +col) % 2 == 0 else theme.bg.light
+                    # label 
+                    lbl = self.config.font.render(Square.get_alphacol(col), 1, color)
+                    lbl_pos = (col * SQSIZE +SQSIZE -20, HEIGHT -20 )
+                    surface.blit(lbl, lbl_pos)
+                    
                 
     def show_piece(self,surface):
         for row in range(ROWS):
--- Chess_Board/src/square.py
+++ Chess_Board/src/square.py
@@ -1,8 +1,10 @@
 class Square:
+    ALPHACOLS  = {0:'a', 1:'b', 2:'c', 3:'d', 4:'e', 5:'f', 6:'g', 7:'h'}
     def __init__(self, row, col, piece = None):
         self.row = row
         self.col = col
         self.piece = piece
+        self.alphacols = self.ALPHACOLS[col]
         
     def __eq__(self, other):
         return self.row == other.row and self.col == other.col
@@ -31,3 +33,9 @@ class Square:
                 return False
             
         return True
+    
+    @staticmethod
+    def get_alphacol(col):
+        ALPHACOLS  = {0:'a', 1:'b', 2:'c', 3:'d', 4:'e', 5:'f', 6:'g', 7:'h'}
+        return ALPHACOLS[col]
+        
