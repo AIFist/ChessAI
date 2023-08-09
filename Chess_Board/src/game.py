73023207eeb42817d84debf6add2cbbf13a0e908
MuhammadNizamani <ishaque.niz786@gmail.com>
2023-08-09 12:48:10 +0500

    EHH:  Sound effects are added

--- Chess_Board/src/game.py
+++ Chess_Board/src/game.py
@@ -86,3 +86,9 @@ class Game:
         
     def change_theme(self):
         self.config.change_theme()
+        
+    def play_sound(self, captured = False):
+        if captured:
+            self.config.capture_sound.play()
+        else:
+            self.config.move_sound.play()
