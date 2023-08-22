1624161e3fb9ce094b75af527304ef534e17b86d
MuhammadNizamani <ishaque.niz786@gmail.com>
2023-08-22 16:43:24 +0500

    second attapet reslove lint

--- Chess_Board/src/piece.py
+++ Chess_Board/src/piece.py
@@ -1,5 +1,7 @@
 import math
 import os
+
+
 class Piece:
     def __init__(self, name, color, value, texture = None, texture_rect= None):
         self.name = name
@@ -21,41 +23,44 @@ class Piece:
         else:
             self.texture = f'../assets/images/imgs-128px/{self.color}_{self.name}.png'
 
-        
+     
     def add_move(self, move):
         self.moves.append(move)
-        
+
+   
     def clear_moves(self):
         self.moves = []
-    
-    
 
 
 class Pawn(Piece):
-    def __init__(self,color):
+    def __init__(self, color):
         self.dir = -1 if color == 'white' else 1
         self.en_passant = False
         super().__init__('pawn', color, 1.0)
-        
+      
 
 class Knight(Piece):
-    def __init__(self,color):
+    def __init__(self, color):
         super().__init__('knight', color, 3.0001)
-        
+
+   
 class Bishop(Piece):
-    def __init__(self,color):
+    def __init__(self, color):
         super().__init__('bishop', color, 3.0)
+ 
     
 class Rook(Piece):
-    def __init__(self,color):
+    def __init__(self, color):
         super().__init__('rook', color, 5.0)
+ 
         
 class Queen(Piece):
-    def __init__(self,color):
+    def __init__(self, color):
         super().__init__('queen', color, 9.0)
+ 
         
 class King(Piece):
-    def __init__(self,color):
+    def __init__(self, color):
         self.left_rook = None
         self.right_rook = None
         super().__init__('king', color, math.inf)
