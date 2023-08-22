6fc60714b49e57c81edf92455e5a141acb3d52b0
MuhammadNizamani <ishaque.niz786@gmail.com>
2023-08-22 16:33:05 +0500

    wokring lint

--- Chess_Board/src/sound.py
+++ Chess_Board/src/sound.py
@@ -1,8 +1,10 @@
 import pygame
+
+
 class Sound:
-    def __init__(self, path) :
+    def __init__(self, path):
         self.path = path
         self.sound = pygame.mixer.Sound(path)
-    
+
     def play(self):
         pygame.mixer.Sound.play(self.sound)
