0a40c4c458feb0fa47e9fbdb21eee6e0127819ef
Salman-18 <salmannizamani205@gmail.com>
2023-12-25 16:24:39 -0800

    fdsfsdf

--- Chess_Board/src/theme.py
+++ Chess_Board/src/theme.py
@@ -1,6 +1,6 @@
 from color import Color
 
-# this theme class
+# this theme class salmanfdsfd
 class Theme:
     def __init__(
         self, light_bg, dark_bg, light_trace, dark_trace, light_moves, dark_moves
